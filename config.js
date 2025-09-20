f3ed70067c9af45403b7537757a0b9043013c21d
thymott10 <tiyovalentino1@gmail.com>
2025-09-20 14:11:19 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "topup_game"
+  },
+  supplierApi: {
+    url: "https://api.supplier.com/order", // dummy
+    key: "SUPPLIER_API_KEY"
+  }
+};
